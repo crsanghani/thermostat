07bc498e901a2c4c7022877856bdc651fdf3ab95
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-25 13:22:33 +0100

    Test written for upButton

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,13 +1,19 @@
 'use strict';
 
 describe('Thermostat', function(){
+  var Thermostat = require('../src/Thermostat');
   var thermostat;
 
   beforeEach(function(){
-    thermostat = new Thermostat();
+   thermostat = new Thermostat();
   });
 
   it('starts at 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases temperature with up button', function(){
+    thermostat.upButton();
+    expect(thermostat.temperature()).toEqual(21);
   });
 });
---
+++ src/Thermostat.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20;
+}
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
+
+
+
+module.exports = Thermostat;
